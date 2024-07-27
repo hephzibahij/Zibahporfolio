bf4363066a842f6471b58d82b2b1e0725a8644e3
Hephzibah <hephzibahijeje89@gmail.com>
2024-07-27 16:08:28 -0400

    files

--- index.js
+++ index.js
@@ -3,51 +3,36 @@
   Have focus outline only for keyboard users 
  ---------------------------------------- */
 
-// index.js
+ const handleMouseDownOnce = () => {
+  document.body.classList.remove('user-is-tabbing')
 
-// JavaScript for hamburger menu toggle
-document.querySelector("#root > div >  > ul")
-
- const handleFirstTab = (e) => {
-    if(e.key === 'Tab') {
-      document.body.classList.add('user-is-tabbing')
-  
-      window.removeEventListener('keydown', handleFirstTab)
-      window.addEventListener('mousedown', handleMouseDownOnce)
-    }
-  
-  }
-  
-  const handleMouseDownOnce = () => {
-    document.body.classList.remove('user-is-tabbing')
-  
-    window.removeEventListener('mousedown', handleMouseDownOnce)
-    window.addEventListener('keydown', handleFirstTab)
-  }
-  
+  window.removeEventListener('mousedown', handleMouseDownOnce)
   window.addEventListener('keydown', handleFirstTab)
-  
-  const backToTopButton = document.querySelector(".back-to-top");
-  let isBackToTopRendered = false;
-  
-  let alterStyles = (isBackToTopRendered) => {
-    backToTopButton.style.visibility = isBackToTopRendered ? "visible" : "hidden";
-    backToTopButton.style.opacity = isBackToTopRendered ? 1 : 0;
-    backToTopButton.style.transform = isBackToTopRendered
-      ? "scale(1)"
-      : "scale(0)";
-  };
-  
-  window.addEventListener("scroll", () => {
-    if (window.scrollY > 700) {
-      isBackToTopRendered = true;
-      alterStyles(isBackToTopRendered);
-    } else {
-      isBackToTopRendered = false;
-      alterStyles(isBackToTopRendered);
-    }
-  });
+}
 
-  function showAccessForm() {
-    document.getElementById('access-form').style.display = 'block';
+window.addEventListener('keydown', handleFirstTab)
+
+const backToTopButton = document.querySelector(".back-to-top");
+let isBackToTopRendered = false;
+
+let alterStyles = (isBackToTopRendered) => {
+  backToTopButton.style.visibility = isBackToTopRendered ? "visible" : "hidden";
+  backToTopButton.style.opacity = isBackToTopRendered ? 1 : 0;
+  backToTopButton.style.transform = isBackToTopRendered
+    ? "scale(1)"
+    : "scale(0)";
+};
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 700) {
+    isBackToTopRendered = true;
+    alterStyles(isBackToTopRendered);
+  } else {
+    isBackToTopRendered = false;
+    alterStyles(isBackToTopRendered);
+  }
+});
+
+function showAccessForm() {
+  document.getElementById('access-form').style.display = 'block';
 }
